b7a6739c9a3e775d8e0232a2e40ceafe3238b5ca
thamizhD <tamizha2915@gmail.com>
2024-12-14 12:01:12 +0530

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './App.css'
+import '../public/App.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ScrollToTop } from './components/ScrollTop.jsx'
 
